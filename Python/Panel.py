a34a9d0e2453fa116b41a606ccc7b9c0087a179f
ceds92 <julian.ceddia@monash.edu>
2022-10-07 12:26:35 +1100

    Minor fixes
     - Help text textbox stops changing size dynamically
     - Added thermal broadening term to Fermi-Dirac fitting

--- Python/Panel.py
+++ Python/Panel.py
@@ -85,7 +85,9 @@ class Panel():
     
     def _helpLabel(self):
         self.helpLabel = ctk.CTkLabel(self.master,text="",justify=tk.LEFT)
-        self.helpLabel.grid(row=12,column=self.pos,columnspan=self.length,rowspan=4)
+        for r in range(4):
+            self.master.rowconfigure(index=r+12,weight=1,minsize=20)
+        self.helpLabel.grid(row=12,column=self.pos,columnspan=self.length,rowspan=4,sticky='nsew')
     
     def removeHelpLabel(self):
         self.helpLabel.grid_forget()
